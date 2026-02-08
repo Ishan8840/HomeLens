a60c02ca821285ae415b2f8543c1d8e459fc06c4
Johnny Li <126725596+jthon10@users.noreply.github.com>
2026-02-07 19:42:45 -0500

    Add files via upload

---
+++ camera-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Camera from './components/Camera';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Camera App</h1>
+      <Camera />
+    </div>
+  );
+}
+
+export default App;
